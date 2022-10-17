3a2182a3d0e79d98176ba07d5a09123ecdac19c8
Dimitar Markov <dimitar.markov@sap.com>
2022-10-17 20:35:50 +0300

    basic react

--- myApp.js
+++ myApp.js
@@ -1,39 +1,18 @@
-let express = require('express');
 let app = express();
 
+function hello(req, res) {
+  absolutePath = __dirname + "/views/index.html";
+  console.log(absolutePath);
+  res.sendFile(absolutePath);
+}
 
+function jsonResponse(req, res) {
+  const obj = {
+    message: "Hello json",
+  };
+  res.json(obj);
+}
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/json", jsonResponse);
+app.get("/", hello);
