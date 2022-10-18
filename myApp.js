d477390d98ad2d9400b437e9476161d9ff67ed74
Dimitar Markov <dimitar.markov@sap.com>
2022-10-18 20:52:23 +0300

    Add test middleware

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+let express = require("express");
 let app = express();
 
 function hello(req, res) {
@@ -7,12 +9,22 @@ function hello(req, res) {
 }
 
 function jsonResponse(req, res) {
+  const message = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
   const obj = {
-    message: "Hello json",
+    message
   };
   res.json(obj);
 }
+
+const logger = function (req, res, next) { 
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+
 app.use("/public", express.static(__dirname + "/public"));
+app.use(logger);
 
 app.get("/json", jsonResponse);
 app.get("/", hello);
+
+module.exports = app;
