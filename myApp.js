a106447751ba0cc53c87dcbc29cf345db2a9e60b
Dimitar Markov <dimitar.markov@sap.com>
2022-10-22 20:47:30 +0300

    Finish basic react

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
 
 function hello(req, res) {
@@ -16,11 +17,33 @@ function jsonResponse(req, res) {
   res.json(obj);
 }
 
+function name(req, res) {
+  const { first, last } = req.query;
+  const obj = {
+    name: `${first} ${last}`
+  };
+  res.json(obj);
+}
+
+function postName(req, res) {
+  const { first, last } = req.body;
+  const obj = {
+    name: `${first} ${last}`
+  };
+  res.json(obj);
+}
+
+const echo = function(req, res) {
+  const { word } = req.params;
+  res.json({ echo: word });
+}
+
 const logger = function (req, res, next) { 
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 }
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(__dirname + "/public"));
 app.use(logger);
 
@@ -29,14 +52,15 @@ app.get(
   function (req, res, next) {
     const time = new Date().toString();
     req.time = time;
-    req.user = getTheUserSync(); // Hypothetical synchronous operation
     next();
   },
   function (req, res) {
     res.json({time: req.time});
   }
 );
+app.get("/name", name).post("/name", postName);
 app.get("/json", jsonResponse);
+app.get("/:word/echo", echo);
 app.get("/", hello);
 
 module.exports = app;
